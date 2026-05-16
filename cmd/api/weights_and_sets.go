a292dbf4d19c0a80ee0c149fb679e0b579c62852
DavidMWeaver4 <davidweaverelt@gmail.com>
2026-05-16 18:15:11 +0900

    7.1 added weights and sets handlers

--- cmd/api/weights_and_sets.go
+++ cmd/api/weights_and_sets.go
@@ -1,3 +1,25 @@
 package main
 
 //TODO
+/*  commented out until finished
+func (cfg *apiConfig) handlerCreateWeightsAndSets(w http.ResponseWriter, r *http.Request) {
+	userID, err := cfg.getUserIDFromToken(w, r)
+	if err != nil {
+		return
+	}
+	type parameters struct {
+		WorkoutExercisesID uuid.UUID `json:"workout_exercises_id"`
+		Weight string `json:"weight"`
+		IsKilogram bool `json:"is_kilogram"`
+
+
+	}
+	decoder := json.NewDecoder(r.Body)
+	var params parameters
+	err = decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error decoding JSON", err)
+		return
+	}
+}
+*/
